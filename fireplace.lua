0110f616a2a114b147726a8ad175aa5087f4640d
riksarchen <andershelin@icloud.com>
2017-02-01 21:25:22 +0100

    Update fireplace.lua

--- fireplace.lua
+++ fireplace.lua
@@ -6,31 +6,31 @@ commandArray = {}
 local tempKamin             = 'Temperatur Kamin'               -- Namnet på temperaturgivaren
 local usrKaminLastTemp      = 'KaminLastTemp'
 local usrKaminTrigger       = 'KaminTrigger'
-local kaminTemp             = 0
-local tempDiff              = 0
 local tempTriggerDec        = 0.175                            -- temperatursänkning i % innan varning triggas 0.175 = 17.5%
 local tempTriggerInc        = 0.40                             -- temperaturökning innan trigger återaktiveras 0.4 = 40% av temperatursänkningen som krävs för att trigga varningen
 
 if (devicechanged[tempKamin]) then
     -- values
-    kaminTemp = tonumber(otherdevices_svalues[tempKamin])
-    tempDiff = kaminTemp * tempTriggerDec * -1
+    local kaminTemp = tonumber(otherdevices_svalues[tempKamin])
+    local tempDiff = kaminTemp * tempTriggerDec * -1
+    local vKaminLastTemp = b.getVar(usrKaminLastTemp)
+    local KaminTrigger = b.getVar(usrKaminTrigger)
     
     -- Påminn om att kaminen håller på att slockna
-    if ((kaminTemp - b.getVar(usrKaminLastTemp)) < tempDiff) then
-        if (b.getVar(usrKaminTrigger) == 1) then
+    if ((kaminTemp - vKaminLastTemp) < tempDiff) then
+        if (KaminTrigger == 1) then
             commandArray['SendNotification']='Kaminen håller på att slockna!#Dags att lägga in mer ved.#0'
             b.setVar(usrKaminTrigger, 0, 0)
-            print("Kaminens temperatur har sjunkit med "..b.getVar(usrKaminLastTemp) - kaminTemp.."c. Dags att lägga i mer ved.")
+            print("Kaminens temperatur har sjunkit med "..vKaminLastTemp - kaminTemp.."c. Dags att lägga i mer ved.")
         end
         b.setVar(usrKaminLastTemp, kaminTemp , 1)
     end
     
     -- återaktivera trigger
-    if ((b.getVar(usrKaminLastTemp) - kaminTemp) < (tempTriggerInc * tempDiff)) then
+    if ((vKaminLastTemp - kaminTemp) < (tempTriggerInc * tempDiff)) then
         b.setVar(usrKaminTrigger, 1, 0)
         b.setVar(usrKaminLastTemp, kaminTemp , 1)
-        print("Kaminens temperatur har ökat med "..kaminTemp - b.getVar(usrKaminLastTemp).."c. Sparar värde till systemet och aktiverar trigger.")
+        print("Kaminens temperatur har ökat med "..kaminTemp - vKaminLastTemp.."c. Sparar värde till systemet och aktiverar trigger.")
     end
 end
 return commandArray
